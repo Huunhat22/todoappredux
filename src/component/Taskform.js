8df7d581bba1e23aa849e0d49587c87ef7a4787c
huunhat22 <huunhatth07@gmail.com>
2021-05-13 00:05:18 +0700

    xử lý sự kiện click ẩn taskForm, và xử lý sự kiện thêm công việc cho Task.

--- src/component/Taskform.js
+++ src/component/Taskform.js
@@ -1,30 +1,62 @@
 import React, {Component} from 'react';
 
 class Taskform extends Component{
+
+    //create constructor
+    constructor(props){
+        super(props);
+        this.state = {
+            Namework : '',
+            Status : false
+        }
+    }
+
+    //function onCloseForm
+    onCloseForm=()=>{
+        this.props.reviceAction();
+    }
+
+    //Function onChangeAction
+    onChangeAction =(event)=>{
+        var target = event.target;
+        var name = target.name;
+        var value = target.value;
+        this.setState({
+            [name] : value
+        })
+    }
+
+    onSubmit = (event) =>{
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render(){
         return(
             /* Col to insert todo */
             <div className="card">
                 <div className="d-flex justify-content-between card-header">
                     <h5 className="text-center">Add New Work</h5>
-                    <a href="/#"><i className="fas fa-times-circle"></i></a>
+                    <span onClick={this.onCloseForm}><i className="fas fa-times-circle"></i></span>
                 </div>
                 <div className="card-body">
-                    <div className="mb-3">
-                        <label className="form-label">Title</label>
-                        <input className="form-control" type="text"></input>
-                    </div>
-                    <div className="mb-3">
-                        <select className="form-select">
-                            <option value={0}>Please Choose The Status</option>
-                            <option value={1}>Active</option>
-                            <option value={2}>Deactivate</option>
-                        </select>
-                    </div>
-                    <div className="d-flex justify-content-evenly mb-3">
-                        <button className="btn btn-outline-success">Save</button>
-                        <button className="btn btn-outline-danger">Delete</button>
-                    </div>
+                    <form onSubmit = {this.onSubmit}>
+                        <div className="mb-3">
+                            <label className="form-label">Title</label>
+                            <input className="form-control" type="text" name="Namework" value={this.state.Namework} onChange = {this.onChangeAction}></input>
+                        </div>
+                        <div className="mb-3">
+                            <select className="form-select" name="Status" value={this.state.Status} onChange = {this.onChangeAction}>
+                                <option>Please Choose The Status</option>
+                                <option value={true}>Active</option>
+                                <option value={false}>Deactivate</option>
+                            </select>
+                        </div>
+                        <div className="d-flex justify-content-evenly mb-3">
+                            <button className="btn btn-outline-success" type="submit">Save</button>
+                            <button className="btn btn-outline-danger">Delete</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         )
