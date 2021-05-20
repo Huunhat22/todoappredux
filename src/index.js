c88c9a3dd686928b99ecfa667a357ddef722a50d
huunhat22 <huunhatth07@gmail.com>
2021-05-20 23:27:19 +0700

    setup react-redux advance

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//Store
+import {createStore} from 'redux';
+import myReducer from './reducers/index';
+import {Provider} from 'react-redux';
+const store = createStore(myReducer);
+
 ReactDOM.render(
-   
-    <App />,
-   
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
+  <Provider store = {store}>
+    <App />
+  </Provider>, 
+   
   document.getElementById('root')
 );
 
